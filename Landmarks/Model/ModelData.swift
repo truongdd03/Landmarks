cc4c6b285aa06db33b761337b3975859372e9d66
Don Truong <Don@Boy-Macbook-Pro.local>
2021-11-03 23:22:40 -0400

    Add files

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Don Truong on 11/3/21.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkdData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("File \(filename) doesn't exist")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load file \(filename)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Fail to parse data from file \(filename)")
+    }
+    
+}
